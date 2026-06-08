2b0c1f23a6289aee90fe78d8a9443332176fafc5
error-ident <35347611+error-ident@users.noreply.github.com>
2026-06-08 16:42:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'wt-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/worktime/', '/worktime/index.html', '/worktime/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
